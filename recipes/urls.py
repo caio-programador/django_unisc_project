8dc959187b43a439f7f6917961594f8c4a7e9b05
= <=>
2024-05-24 09:26:14 -0300

    excluindo porque não precisa

--- recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('contato/', views.contato, name="contato"),
-    path('sobre/', views.sobre, name="sobre"),
-    path('teste/', views.teste, name="sobre"),
-]
